a961d8496f5a9fd5ce59229539fbf70b44346034
marmooo <marmooo@users.noreply.github.com>
2026-09-11 21:20:52 +0900

    improve build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 mkdir -p docs
 cp -r src/* docs
 drop-inline-css -r src -o docs
+deno bundle --allow-import \
+  --platform=browser \
+  --format=esm \
+  -o docs/index.js \
+  --external=mediabunny \
+  --external=https://cdn.jsdelivr.net/* \
+  --external=https://cdn.jsdelivr.net/gh/* \
+  src/index.js
 minify -r docs -o .
